d5ac2848f6dbbcfc8ae8a37deed8a4042f090af5
iln <romanhvorostov87@gmail.com>
2019-07-22 22:25:57 +0700

    Delivered TextAsset loading functionality to a separate class. Little refactor. Add CSV file example.

---
+++ Assets/Localizator/Scripts/Parses/LocalizeCSV.cs
@@ -0,0 +1,27 @@
+﻿namespace LocalizatorSystem
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+
+    
+    /// <summary>
+    /// CSV parser that takes the necessary parameters from the localization files
+    /// </summary>
+    public class LocalizeCSV : IParseableLocalize
+    {
+        public List<string> AvailableLanguages {get; set;}
+        public Dictionary<string, string> ParsedLocalization { get; set; }
+
+        private const string PATH = "LocCSV";
+        public void InitParseModule(string currentLanguage)
+        {
+            TextAsset textFromFile = TextAssetFromFile.GetTextAsset(PATH);
+            ParseCSV(textFromFile.text);
+        }
+
+        private void ParseCSV(string locText)
+        {
+
+        }
+    }
+}
